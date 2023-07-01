4a78191603f95ffb6d4d857e896159dbdb3446d4
이민수 <mns0304@cashwalk.io>
2023-07-01 20:28:03 +0900

    fix(viewBinding) : MainActivity viewbinding 적용

--- app/src/main/java/com/jackson/swipecomparison/MainActivity.kt
+++ app/src/main/java/com/jackson/swipecomparison/MainActivity.kt
@@ -2,10 +2,17 @@ package com.jackson.swipecomparison
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jackson.swipecomparison.databinding.ActivityMainBinding
+import com.jackson.swipecomparison.extensions.setContentView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding)
     }
 }
---
+++ app/src/main/java/com/jackson/swipecomparison/extensions/BindingExtension.kt
@@ -0,0 +1,9 @@
+package com.jackson.swipecomparison.extensions
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+fun <T: ViewBinding> AppCompatActivity.setContentView(binding: T): T {
+    binding.root.let(::setContentView)
+    return binding
+}
