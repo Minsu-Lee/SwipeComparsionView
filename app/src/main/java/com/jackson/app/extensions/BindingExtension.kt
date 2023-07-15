d7bd999679bd131babaf5bb4798893b287a5118b
이민수 <mns0304@cashwalk.io>
2023-07-16 00:16:06 +0900

    fix(module) : app module 삭제

--- app/src/main/java/com/jackson/app/extensions/BindingExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.jackson.app.extensions
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.viewbinding.ViewBinding
-
-fun <T: ViewBinding> AppCompatActivity.setContentView(binding: T): T {
-    binding.root.let(::setContentView)
-    return binding
-}
